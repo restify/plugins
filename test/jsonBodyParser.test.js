3a70f09abe30b7fee5a6d00693e07ffbd97406b8
Alex Liu <donutespresso@gmail.com>
2015-09-09 18:57:55 -0700

    more unit test cleanup

--- test/jsonBodyParser.test.js
+++ test/jsonBodyParser.test.js
@@ -1,5 +1,8 @@
 'use strict';
 
+// core requires
+var net = require('net');
+
 // external requires
 var assert = require('chai').assert;
 var restify = require('restify');
@@ -12,6 +15,7 @@ var plugins = require('../lib');
 // local globals
 var SERVER;
 var CLIENT;
+var STRING_CLIENT;
 var PORT;
 
 
@@ -31,6 +35,14 @@ describe('JSON body parser', function () {
                 retry: false,
                 agent: false
             });
+            STRING_CLIENT = restifyClients.createStringClient({
+                url: 'http://127.0.0.1:' + PORT,
+                dtrace: helper.dtrace,
+                retry: false,
+                agent: false,
+                contentType: 'application/json',
+                accept: 'application/json'
+            });
 
             done();
         });
@@ -38,6 +50,28 @@ describe('JSON body parser', function () {
 
     afterEach(function (done) {
         SERVER.close(done);
+        CLIENT.close();
+        STRING_CLIENT.close();
+    });
+
+    it('should parse null JSON body', function (done) {
+        SERVER.use(plugins.jsonBodyParser({
+            mapParams: true
+        }));
+
+        SERVER.post('/body/:id', function (req, res, next) {
+            assert.equal(req.params.id, 'foo');
+            assert.equal(req.body, null);
+            res.send();
+            next();
+        });
+
+        STRING_CLIENT.post('/body/foo?name=markc', 'null',
+        function (err, _, res) {
+            assert.notOk(err);
+            assert.equal(res.statusCode, 200);
+            done();
+        });
     });
 
     it('should parse empty JSON body', function (done) {
@@ -181,6 +215,119 @@ describe('JSON body parser', function () {
         });
     });
 
+    it('GH-318 get request with body (default)', function (done) {
+        SERVER.use(plugins.bodyParser({
+            mapParams: true
+        }));
+
+        SERVER.get('/getWithoutBody',
+            function (req, res, next) {
+                assert.notEqual(req.params.foo, 'bar');
+                res.send();
+                next();
+            });
+
+        var request = 'GET /getWithoutBody HTTP/1.1\r\n' +
+            'Content-Type: application/json\r\n' +
+            'Content-Length: 13\r\n' +
+            '\r\n' +
+            '{"foo":"bar"}';
+
+        var client = net.connect({host: '127.0.0.1', port: PORT}, function () {
+            client.write(request);
+        });
+        client.once('data', function (data) {
+            client.end();
+        });
+        client.once('end', function () {
+            done();
+        });
+    });
+
+    it('GH-318 get request with body (requestBodyOnGet=true)', function (done) {
+        SERVER.use(plugins.bodyParser({
+            mapParams: true,
+            requestBodyOnGet: true
+        }));
+
+        SERVER.get('/getWithBody', function (req, res, next) {
+            assert.equal(req.params.foo, 'bar');
+            res.send();
+            next();
+        });
+
+        var request = 'GET /getWithBody HTTP/1.1\r\n' +
+            'Content-Type: application/json\r\n' +
+            'Content-Length: 13\r\n' +
+            '\r\n' +
+            '{"foo":"bar"}';
+
+        var client = net.connect({host: '127.0.0.1', port: PORT}, function () {
+            client.write(request);
+        });
+
+        client.once('data', function (data) {
+            client.end();
+        });
+
+        client.once('end', function () {
+            done();
+        });
+    });
+
+    it('GH-111 JSON Parser not right for arrays', function (done) {
+        SERVER.use(plugins.bodyParser({
+            mapParams: true
+        }));
+
+        SERVER.post('/gh111', function (req, res, next) {
+            assert.ok(Array.isArray(req.params));
+            assert.equal(req.params[0], 'foo');
+            assert.equal(req.params[1], 'bar');
+            res.send();
+            next();
+        });
+
+        var obj = ['foo', 'bar'];
+        CLIENT.post('/gh111', obj, function (err, _, res) {
+            assert.notOk(err);
+            assert.equal(res.statusCode, 200);
+            done();
+        });
+    });
+
+    it('GH-279 more JSON Arrays', function (done) {
+        SERVER.use(plugins.jsonBodyParser({
+            mapParams: true
+        }));
+
+        SERVER.post('/gh279', function respond(req, res, next) {
+            assert.ok(Array.isArray(req.params));
+            assert.equal(req.params[0].id, '123654');
+            assert.ok(req.params[0].name, 'mimi');
+            assert.ok(req.params[1].id, '987654');
+            assert.ok(req.params[1].name, 'pijama');
+            res.send(200);
+            next();
+        });
+
+        var obj = [
+            {
+                id: '123654',
+                name: 'mimi'
+            },
+            {
+                id: '987654',
+                name: 'pijama'
+            }
+        ];
+        CLIENT.post('/gh279', obj, function (err, _, res) {
+            assert.notOk(err);
+            assert.equal(res.statusCode, 200);
+            done();
+        });
+    });
 });
+
 
 
