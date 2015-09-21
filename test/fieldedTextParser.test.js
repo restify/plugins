11989413e2f0a70be35c4dc1dce4c9553433bfb0
Alex Liu <donutespresso@gmail.com>
2015-09-20 18:28:41 -0700

    migrate unit tests to mocha. mapParams: false is now default for query and body parsers

--- test/fieldedTextParser.test.js
+++ test/fieldedTextParser.test.js
@@ -1,136 +1,125 @@
-/**
- * Module dependencies
- */
-
 'use strict';
 
-if (require.cache[__dirname + '/lib/helper.js']) {
-    delete require.cache[__dirname + '/lib/helper.js'];
-}
-
-var restify = require('../lib');
-var restifyClients = require('restify-clients');
+// core requires
 var fs = require('fs');
-var helper = require('./lib/helper.js');
-var before = helper.before;
-var after = helper.after;
-var test = helper.test;
+var path = require('path');
 
-/**
- * Globals
- */
+// external requires
+var assert = require('chai').assert;
+var restify = require('restify');
+var restifyClients = require('restify-clients');
+
+// local files
+var plugins = require('../lib');
+var helper = require('./lib/helper');
 
 var fsOptions = { encoding: 'utf8' };
-
 var PORT = process.env.UNIT_TEST_PORT || 3333;
 var CLIENT;
 var SERVER;
-var DATA_CSV = fs.readFileSync(__dirname + '/files/data-csv.txt', fsOptions);
-var DATA_TSV = fs.readFileSync(__dirname + '/files/data-tsv.txt', fsOptions);
-var OBJECT_CSV = require(__dirname + '/files/object-csv.json');
-var OBJECT_TSV = require(__dirname + '/files/object-tsv.json');
+var DATA_CSV = fs.readFileSync(
+    path.join(__dirname, '/files/data-csv.txt'),
+    fsOptions
+);
+var DATA_TSV = fs.readFileSync(
+    path.join(__dirname, '/files/data-tsv.txt'),
+    fsOptions
+);
+var OBJECT_CSV = require(path.join(__dirname, '/files/object-csv.json'));
+var OBJECT_TSV = require(path.join(__dirname, '/files/object-tsv.json'));
 
 /**
  * Tests
  */
 
-before(function (callback) {
-    try {
+describe('fielded text parser', function () {
+
+    before(function (done) {
         SERVER = restify.createServer({
             dtrace: helper.dtrace,
             log: helper.getLog('server')
         });
-        SERVER.use(restify.acceptParser(SERVER.acceptable));
-        SERVER.use(restify.authorizationParser());
-        SERVER.use(restify.dateParser());
-        SERVER.use(restify.queryParser());
-        SERVER.use(restify.bodyParser());
+        SERVER.use(plugins.bodyParser());
         SERVER.post('/data', function respond(req, res, next) {
             res.send({
                 status: 'okay',
                 parsedReq: req.body
             });
-            return (next());
+            return next();
         });
         SERVER.listen(PORT, '127.0.0.1', function () {
             CLIENT = restifyClients.createClient({
                 url: 'http://127.0.0.1:' + PORT,
                 dtrace: helper.dtrace,
-                retry: false,
-                agent: false
+                retry: false
             });
-            callback();
+            done();
         });
-    } catch (e) {
-        console.error(e.stack);
-        process.exit(1);
-    }
-});
+    });
 
-after(function (callback) {
-    try {
+    after(function (done) {
         CLIENT.close();
-        SERVER.close(callback);
-    } catch (e) {
-        console.error(e.stack);
-        process.exit(1);
-    }
+        SERVER.close(done);
+    });
+
+
+    it('should parse CSV body', function (done) {
+        var options = {
+            path: '/data',
+            headers: {
+                'Content-Type': 'text/csv'
+            }
+        };
+        CLIENT.post(options, function (err, req) {
+            assert.ifError(err);
+            req.on('result', function (errReq, res) {
+                assert.ifError(errReq);
+                res.body = '';
+                res.setEncoding('utf8');
+                res.on('data', function (chunk) {
+                    res.body += chunk;
+                });
+                res.on('end', function () {
+                    res.body = JSON.parse(res.body);
+                    var parsedReqStr = JSON.stringify(res.body.parsedReq);
+                    var objectStr = JSON.stringify(OBJECT_CSV);
+                    assert.equal(parsedReqStr, objectStr);
+                    done();
+                });
+            });
+            req.write(DATA_CSV);
+            req.end();
+        });
+    });
+
+    it('should parse TSV body', function (done) {
+        var options = {
+            path: '/data',
+            headers: {
+                'Content-Type': 'text/tsv'
+            }
+        };
+        CLIENT.post(options, function (err, req) {
+            assert.ifError(err);
+            req.on('result', function (errReq, res) {
+                assert.ifError(errReq);
+                res.body = '';
+                res.setEncoding('utf8');
+                res.on('data', function (chunk) {
+                    res.body += chunk;
+                });
+                res.on('end', function () {
+                    res.body = JSON.parse(res.body);
+                    var parsedReqStr = JSON.stringify(res.body.parsedReq);
+                    var objectStr = JSON.stringify(OBJECT_TSV);
+                    assert.equal(parsedReqStr, objectStr);
+                    done();
+                });
+            });
+            req.write(DATA_TSV);
+            req.end();
+        });
+    });
+
 });
 
-test('Parse CSV body', function (t) {
-    var options = {
-        path: '/data',
-        headers: {
-            'Content-Type': 'text/csv'
-        }
-    };
-    CLIENT.post(options, function (err, req) {
-        t.ifError(err);
-        req.on('result', function (errReq, res) {
-            t.ifError(errReq);
-            res.body = '';
-            res.setEncoding('utf8');
-            res.on('data', function (chunk) {
-                res.body += chunk;
-            });
-            res.on('end', function () {
-                res.body = JSON.parse(res.body);
-                var parsedReqStr = JSON.stringify(res.body.parsedReq);
-                var objectStr = JSON.stringify(OBJECT_CSV);
-                t.equal(parsedReqStr, objectStr);
-                t.end();
-            });
-        });
-        req.write(DATA_CSV);
-        req.end();
-    });
-});
-
-test('Parse TSV body', function (t) {
-    var options = {
-        path: '/data',
-        headers: {
-            'Content-Type': 'text/tsv'
-        }
-    };
-    CLIENT.post(options, function (err, req) {
-        t.ifError(err);
-        req.on('result', function (errReq, res) {
-            t.ifError(errReq);
-            res.body = '';
-            res.setEncoding('utf8');
-            res.on('data', function (chunk) {
-                res.body += chunk;
-            });
-            res.on('end', function () {
-                res.body = JSON.parse(res.body);
-                var parsedReqStr = JSON.stringify(res.body.parsedReq);
-                var objectStr = JSON.stringify(OBJECT_TSV);
-                t.equal(parsedReqStr, objectStr);
-                t.end();
-            });
-        });
-        req.write(DATA_TSV);
-        req.end();
-    });
-});
