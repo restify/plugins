ad04f328378097cb33b01fc8a95e7ca40423ed87
Brian Aabel <brian.aabel@gmail.com>
2016-07-25 11:39:49 -0400

    Updated to conform to The OAuth 2.0 Authorization Framework: Bearer Token Usage
    Specifically updated to fix the following:
    
    According to RFC we are missing a few validation/verification checks:
    
    "Clients MUST NOT use more than one method to transmit the token in each request." (Section 2 - you should probably return a 400).
    
    "The HTTP request entity-header includes the "Content-Type" header field set to "application/x-www-form-urlencoded"" (Section 2.2 - don't set the token if this isn't true when inspecting req.body)
    
    REMOVED URI QUERY STRING PARSING DUE TO SECURITY CONCERNS
    "Clients using the URI Query Parameter method SHOULD also send a Cache-Control header containing the "no-store" option. Server success (2XX status) responses to these requests SHOULD contain a Cache-Control header with the "private" option." (Section 2.3)

--- test/oauth2.test.js
+++ test/oauth2.test.js
@@ -2,6 +2,9 @@
 
 'use strict';
 
+
+// core requires
+var http = require('http');
 // external requires
 var assert = require('chai').assert;
 var restify = require('restify');
@@ -15,6 +18,7 @@ var plugins = require('../lib');
 var SERVER;
 var CLIENT;
 var PORT;
+var TEST_TOKEN = '18926970-A-nMnSHDqg8Fsunm6Qx1cF1APp';
 
 describe('oauth2 token parser', function () {
 
@@ -55,12 +59,12 @@ describe('oauth2 token parser', function () {
         var opts = {
             path: '/',
             headers: {
-                Authorization : 'Bearer cn389ncoiwuencr'
+                Authorization : 'Bearer ' + TEST_TOKEN
             }
         };
         SERVER.get('/query/:id', function (req, res, next) {
             assert.isNotNull(req.oauth2.accessToken);
-            assert.equal(req.oauth2.accessToken, 'cn389ncoiwuencr');
+            assert.equal(req.oauth2.accessToken, TEST_TOKEN);
             res.send();
             next();
         });
@@ -74,14 +78,13 @@ describe('oauth2 token parser', function () {
 
     });
 
-    it('should parse oauth2 token from query string', function (done) {
+    it('should do nothing (token is null) if there is no oauth2 token set', function (done) {
         var opts = {
-            path: '/?access_token=cn389ncoiwuencr'
+            path: '/test'
 
         };
         SERVER.get(opts, function (req, res, next) {
-            assert.isNotNull(req.oauth2.accessToken);
-            assert.equal(req.oauth2.accessToken, 'cn389ncoiwuencr');
+            assert.isNull(req.oauth2.accessToken);
             assert.equal(res.statusCode, 200);
             res.send();
             next();
@@ -94,20 +97,98 @@ describe('oauth2 token parser', function () {
 
     });
 
-    it('should parse oauth2 token from request body', function (done) {
+    it('should parse oauth2 token from request body when content type is application/x-www-form-urlencoded', function (done) {
 
 
         SERVER.post('/', function (req, res, next) {
             assert.isNotNull(req.oauth2.accessToken);
-            assert.equal(req.oauth2.accessToken, 'cn389ncoiwuencr');
+            assert.equal(req.oauth2.accessToken, TEST_TOKEN);
             res.send();
             next();
         });
-        CLIENT.post('/', {access_token: 'cn389ncoiwuencr'},
+
+        var opts = {
+            path: '/',
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded'
+            }
+        }
+
+        CLIENT.post('/', {access_token: TEST_TOKEN},
             function (err, _, res) {
             assert.ifError(err);
             assert.equal(res.statusCode, 200);
             done();
+        });
+
+    });
+
+    it('should parse oauth2 token from request body when content type is APPLICATION/x-www-form-urlencoded (case-insensitive)', function (done) {
+
+
+        SERVER.post('/', function (req, res, next) {
+            assert.isNotNull(req.oauth2.accessToken);
+            assert.equal(req.oauth2.accessToken, TEST_TOKEN);
+            res.send();
+            next();
+        });
+
+        var opts = {
+            path: '/',
+            headers: {
+                'Content-Type': 'APPLICATION/x-www-form-urlencoded'
+            }
+        }
+
+        CLIENT.post('/', {access_token: TEST_TOKEN},
+            function (err, _, res) {
+            assert.ifError(err);
+            assert.equal(res.statusCode, 200);
+            done();
+        });
+
+    });
+
+    it('should ignore token from request body if content type is NOT application/x-www-form-urlencoded', function (done) {
+
+
+        SERVER.post('/', function (req, res, next) {
+            assert.isNull(req.oauth2.accessToken);
+            res.send();
+            next();
+        });
+
+        CLIENT.post('/', {access_token: TEST_TOKEN},
+            function (err, _, res) {
+            assert.ifError(err);
+            assert.equal(res.statusCode, 200);
+            done();
+        });
+
+    });
+
+
+    it('should fail if more than one method is used to set the oauth2 token', function (done) {
+
+
+        SERVER.post('/', function (req, res, next) {
+            assert.isNull(req.oauth2.accessToken);
+            res.send();
+            next();
+        });
+        var opts = {
+            path: '/',
+            headers: {
+                Authorization : 'Bearer ' + TEST_TOKEN,
+                'Content-Type': 'application/x-www-form-urlencoded'
+            }
+        }
+
+        CLIENT.post(opts, {access_token: TEST_TOKEN},
+            function (err, _, res) {
+                assert.ok(err);
+                assert.equal(res.statusCode, 400);
+                done();
         });
 
     });
